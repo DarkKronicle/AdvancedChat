ad1a752f0919bb67c06b485c2856c012750f21ac
diademiemi <emilia@diademiemi.me>
2021-06-19 00:52:58 +0200

    [WIP] Added addLabel, compiles but doesn't show Configure in menu

--- src/main/java/io/github/darkkronicle/advancedchat/filters/processors/CommandRunProcessor.java
+++ src/main/java/io/github/darkkronicle/advancedchat/filters/processors/CommandRunProcessor.java
@@ -1,5 +1,7 @@
 package io.github.darkkronicle.advancedchat.filters.processors;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import fi.dy.masa.malilib.config.IConfigBase;
 import fi.dy.masa.malilib.config.options.ConfigInteger;
 import fi.dy.masa.malilib.config.options.ConfigString;
@@ -10,7 +12,10 @@ import fi.dy.masa.malilib.gui.button.ButtonBase;
 import fi.dy.masa.malilib.gui.button.ButtonGeneric;
 import fi.dy.masa.malilib.gui.button.IButtonActionListener;
 import fi.dy.masa.malilib.util.StringUtils;
+import io.github.darkkronicle.advancedchat.interfaces.IJsonApplier;
 import io.github.darkkronicle.advancedchat.interfaces.IMatchProcessor;
+import io.github.darkkronicle.advancedchat.interfaces.IScreenSupplier;
+import io.github.darkkronicle.advancedchat.util.ColorUtil;
 import io.github.darkkronicle.advancedchat.util.FluidText;
 import io.github.darkkronicle.advancedchat.util.SearchResult;
 import io.github.darkkronicle.advancedchat.util.SearchUtils;
@@ -27,9 +32,10 @@ import net.minecraft.network.MessageType;
 
 import java.util.List;
 import javax.annotation.Nullable;
+import java.util.function.Supplier;
 
 @Environment(EnvType.CLIENT)
-public class CommandRunProcessor implements IMatchProcessor {
+public class CommandRunProcessor implements IMatchProcessor, IJsonApplier {
 
   private static String translate(String key) {
       return "advancedchat.config.processor.commandrun."  + key;
@@ -43,6 +49,26 @@ public class CommandRunProcessor implements IMatchProcessor {
               String content = search.getGroupReplacements(command.config.getStringValue(), true);
               return Result.getFromBool(true);
           }
+
+              @Override
+            public JsonObject save() {
+                JsonObject obj = new JsonObject();
+                obj.add(command.key, command.config.getAsJsonElement());
+                return obj;
+            }
+
+            @Override
+            public void load(JsonElement element) {
+                if (element == null || !element.isJsonObject()) {
+                    return;
+                }
+                JsonObject obj = element.getAsJsonObject();
+                command.config.setValueFromJsonElement(obj.get(command.key));
+            }
+
+            public Supplier<Screen> getScreen(@Nullable Screen parent) {
+                return () -> new SenderScreen(parent);
+            }
 
           public class SenderScreen extends GuiBase {
 
@@ -83,11 +109,21 @@ public class CommandRunProcessor implements IMatchProcessor {
                   ButtonGeneric button = new ButtonGeneric(x, y, nameW, 20, name);
                   this.addButton(button, new ButtonListener(ButtonListener.Type.BACK, this));
                   y += 30;
+                  y += this.addLabel(x, y, command.config) + 1;
                   textField = new GuiTextFieldGeneric(x, y, getWidth(), 20, MinecraftClient.getInstance().textRenderer);
                   textField.setMaxLength(64000);
                   textField.setText(command.config.getStringValue());
                   this.addTextField(textField, null);
               }
+
+              private int addLabel(int x, int y, IConfigBase config) {
+                  int width = StringUtils.getStringWidth(config.getConfigGuiDisplayName());
+                  WidgetLabelHoverable label = new WidgetLabelHoverable(x, y, width, 8, ColorUtil.WHITE.color(), config.getConfigGuiDisplayName());
+                  label.setHoverLines(StringUtils.translate(config.getComment()));
+                  this.addWidget(label);
+                  return 8;
+              }
+
 
       public Result processMatches(FluidText text, FluidText unfiltered, SearchResult matches) {
           MinecraftClient client = MinecraftClient.getInstance();
@@ -104,11 +140,11 @@ public class CommandRunProcessor implements IMatchProcessor {
       }
 
 
-    public void back() {
-        this.closeGui(true);
-    }
+        public void back() {
+            this.closeGui(true);
+        }
 
-}
+    }
 
     public static class ButtonListener implements IButtonActionListener {
 
